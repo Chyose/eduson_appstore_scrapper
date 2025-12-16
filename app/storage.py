461691678df51cbe15eea9144ad7926cd52a096d
Pavel <80091598+Chyose@users.noreply.github.com>
2025-12-17 00:37:04 +0300

    Update storage.py

--- app/storage.py
+++ app/storage.py
@@ -13,31 +13,22 @@ class Storage:
 
     @staticmethod
     def save_reviews_md(app_name: str, reviews: List[Review]) -> None:
-        """
-        Сохраняет список отзывов в человекочитаемый Markdown файл
-
-        Args:
-            app_name (str): название приложения
-            reviews (List[Review]): список отзывов
-        """
         Storage.OUTPUT_FILE.parent.mkdir(parents=True, exist_ok=True)
 
         try:
             with Storage.OUTPUT_FILE.open("w", encoding="utf-8") as f:
-                # Заголовок с названием приложения
                 f.write(f"# Отзывы приложения: {app_name}\n")
                 f.write(f"Общее количество отзывов: {len(reviews)}\n\n")
 
                 for review in reviews:
-                    # Заголовок с рейтингом
                     stars = "⭐" * review.rating + "☆" * (5 - review.rating)
+                    date_str = review.date.strftime('%Y-%m-%d') if review.date else "Неизвестно"
                     f.write("---\n")
                     f.write(f"### {stars}\n")
                     f.write(f"**Автор:** {review.author}\n")
-                    f.write(f"**Дата:** {review.date.strftime('%Y-%m-%d') if review.date else 'Неизвестно'}\n")
+                    f.write(f"**Дата:** {date_str}\n")
                     f.write("**Текст:**\n")
                     f.write(f"{review.content}\n\n")
-
             logger.info(f"Отзывы успешно сохранены в {Storage.OUTPUT_FILE}")
         except Exception as e:
             logger.error(f"Ошибка при сохранении отзывов: {e}")
