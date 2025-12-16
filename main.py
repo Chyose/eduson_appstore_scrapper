102855c728d0badb3c5b8a96c4b170c118d496f3
Pavel <80091598+Chyose@users.noreply.github.com>
2025-12-17 00:20:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+from app.service import AppStoreService
+from app.storage import MarkdownStorage
+
+service = AppStoreService()
+storage = MarkdownStorage()
+
+reviews, app_name = service.get_reviews("570060128")  # пример App ID
+storage.save_reviews(app_name, reviews)
 
