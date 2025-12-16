e63b4999289f7b19b04f3e13f02c2fe71aba4643
Pavel <80091598+Chyose@users.noreply.github.com>
2025-12-17 01:41:58 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,11 +1,16 @@
-# app/models.py
 from dataclasses import dataclass
+from typing import Optional
 
 @dataclass
 class Review:
-    """DTO для одного отзыва App Store."""
     author: str
     date: str
     title: str
-    rating: int
     content: str
+    rating: int
+
+    def is_valid(self) -> bool:
+        """Проверка, что отзыв содержит хотя бы один значимый элемент"""
+        if self.author in ("", "Unknown") and self.date in ("", "Unknown") and not self.content.strip():
+            return False
+        return True
