94beaafcb4bf0b178e810aa2783eb6c10993e768
Pavel <80091598+Chyose@users.noreply.github.com>
2025-12-17 00:28:17 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,8 +1,13 @@
+# app/models.py
 from dataclasses import dataclass
+from datetime import datetime
+from typing import Optional
 
 @dataclass
 class Review:
     author: str
-    date: str
-    rating: str
-    text: str
+    title: str
+    content: str
+    rating: int
+    date: datetime
+    version: Optional[str] = None
