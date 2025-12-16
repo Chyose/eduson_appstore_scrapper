ba547adc2a7f87972c1307106b200201ddbd6efe
Pavel <80091598+Chyose@users.noreply.github.com>
2025-12-17 00:10:45 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,1 +1,12 @@
+from dataclasses import dataclass
+from datetime import date
+
+
+@dataclass(slots=True)
+class Review:
+    author: str
+    rating: int
+    title: str
+    body: str
+    review_date: date
 
